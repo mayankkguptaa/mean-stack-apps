bf3d0596e55c289e80a1e5fd285e6d5399d1aaf4
Satvik <dagasatvik10@gmail.com>
2016-07-09 18:01:03 +0530

    Selection of category for a particular month done...not complete.....designing not done

--- modules/training/client/training.client.module.js
+++ modules/training/client/training.client.module.js
@@ -1,7 +1,7 @@
 (function (app) {
   'use strict';
 
-  app.registerModule('training', ['underscore']);
+  app.registerModule('training', ['underscore', 'core']);
   // app.registerModule('training.services');
   app.registerModule('training.routes', ['ui.router', 'core.routes', 'categories.services']);
 }(ApplicationConfiguration));
