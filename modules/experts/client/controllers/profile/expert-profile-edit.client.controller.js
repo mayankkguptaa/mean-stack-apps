9d0c3252b3ac474411f5acafa412257fefc1f724
Satvik <dagasatvik10@gmail.com>
2016-07-22 13:47:38 +0530

    expert profile show and edit done.....

--- modules/experts/client/controllers/profile/expert-profile-edit.client.controller.js
+++ modules/experts/client/controllers/profile/expert-profile-edit.client.controller.js
@@ -5,9 +5,43 @@
     .module('experts')
     .controller('ExpertProfileEditController', ExpertProfileEditController);
 
-  ExpertProfileEditController.$inject = [];
+  ExpertProfileEditController.$inject = ['$scope', '$http', '$location', 'Authentication', 'ExpertsService'];
 
-  function ExpertProfileEditController() {
+  function ExpertProfileEditController($scope, $http, $location, Authentication, ExpertsService) {
     var vm = this;
+
+    vm.user = Authentication.user;
+    vm.expert = {};
+    vm.updateExpertProfile = updateExpertProfile;
+
+    getExpert();
+
+    function getExpert() {
+      ExpertsService.get(function (res) {
+        vm.expert = res;
+      });
+    }
+
+    // update expert profile
+    function updateExpertProfile(isValid) {
+      vm.success = vm.error = null;
+
+      if (!isValid) {
+        $scope.$broadcast('show-errors-check-validity', 'vm.expertForm');
+
+        return false;
+      }
+
+      var expert = new ExpertsService(vm.expert);
+
+      expert.$update(function (response) {
+        $scope.$broadcast('show-errors-reset', 'vm.expertForm');
+
+        vm.success = true;
+        vm.expert = response;
+      }, function (response) {
+        vm.error = response.data.message;
+      });
+    }
   }
 }());
