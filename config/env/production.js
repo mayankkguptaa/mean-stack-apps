02261febf94c446cf90ce2300368b655fc9ca6cd
Satvik <dagasatvik10@gmail.com>
2016-07-26 15:41:41 +0530

    Merged with satvik....production.js changed....

--- config/env/production.js
+++ config/env/production.js
@@ -13,8 +13,8 @@ module.exports = {
   db: {
     uri: process.env.MONGOHQ_URL || process.env.MONGOLAB_URI || 'mongodb://' + (process.env.DB_1_PORT_27017_TCP_ADDR || 'localhost') + '/mean',
     options: {
-      user: '',
-      pass: ''
+      user: process.env.MONGO_USER,
+      pass: process.env.MONGO_PASS
     },
     // Enable mongoose debug mode
     debug: process.env.MONGODB_DEBUG || false
@@ -31,6 +31,7 @@ module.exports = {
       json: false
     }
   },
+  sessionSecret: process.env.SESSION_SECRET || 'VANITY',
   facebook: {
     clientID: process.env.FACEBOOK_ID || 'APP_ID',
     clientSecret: process.env.FACEBOOK_SECRET || 'APP_SECRET',
